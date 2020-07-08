a62aadc76a3c8094918bbbafd8f8003e144a6c38
almoner <almoner@yandex.ru>
2020-07-08 17:46:09 +0300

    added product and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
-$(function(){
+$(function () {
+
+    $('.product-slider__inner').slick({
+        dots:true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+    });
+
+    var mixer = mixitup('.product__inner-box');
 
 
 });
